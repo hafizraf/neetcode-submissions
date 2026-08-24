89a0b8434c2739af24f28ccd976ccce44c7dadd0
Hafiz Rafeeq <91043653+hafizraf@users.noreply.github.com>
2026-08-24 21:00:12 +0530

    Add: string-encode-and-decode - submission-3

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-3.cs
@@ -0,0 +1,28 @@
+public class Solution {
+
+    public string Encode(IList<string> strs) {
+        StringBuilder encoded = new StringBuilder();
+        foreach(string s in strs)
+        {
+            encoded.Append(s.Length);
+            encoded.Append("#");
+            encoded.Append(s);
+        }
+        return encoded.ToString();
+    }
+
+    public List<string> Decode(string s) {
+        List<string> res = new List<string>();
+        for(int i = 0; i < s.Length; )
+        {
+            int count = 0;
+            while(s[i] != '#')
+            {
+                count = count * 10 + (s[i++] - '0');
+            }
+            res.Add(s.Substring(++i, count));
+            i+= count;
+        }
+        return res;
+   }
+}
