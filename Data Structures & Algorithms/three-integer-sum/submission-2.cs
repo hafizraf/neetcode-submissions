d6512f032dbfaaaf17a8794fd511b02abdf28bfc
Hafiz Rafeeq <91043653+hafizraf@users.noreply.github.com>
2026-08-25 22:49:33 +0530

    Add: three-integer-sum - submission-2

---
+++ Data Structures & Algorithms/three-integer-sum/submission-2.cs
@@ -0,0 +1,42 @@
+public class Solution {
+    public List<List<int>> ThreeSum(int[] nums) {
+        List<List<int>> res = new List<List<int>>();
+        Array.Sort(nums);
+        for(int left = 0; left < nums.Length - 2; left++)
+        {
+            int mid = left+1;
+            int right = nums.Length - 1;
+            while(mid < right)
+            {
+                int sum = nums[left] + nums[mid] + nums[right];
+                if(sum == 0)
+                {
+                    res.Add(new List<int>{nums[left],nums[mid],nums[right]});
+                    while(mid < right && nums[mid] == nums[mid + 1])
+                    {
+                        mid++;
+                    }
+                    while(right > mid && nums[right] == nums[right-1])
+                    {
+                        right--;
+                    }
+                    mid++;
+                    right--;
+                }
+                else if(sum < 0)
+                {
+                    mid++;
+                }
+                else
+                {
+                    right--;
+                }
+            }
+            while(left < nums.Length - 2 && nums[left] == nums[left + 1])
+            {
+                left++;
+            }
+        }
+        return res;
+    }
+}
