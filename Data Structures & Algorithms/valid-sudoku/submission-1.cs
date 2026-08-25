d3445dfa62daadbf09498f361225eb194af65b1a
Hafiz Rafeeq <91043653+hafizraf@users.noreply.github.com>
2026-08-25 20:47:15 +0530

    Add: valid-sudoku - submission-1

---
+++ Data Structures & Algorithms/valid-sudoku/submission-1.cs
@@ -0,0 +1,40 @@
+public class Solution {
+    public bool IsValidSudoku(char[][] board) {
+        HashSet<char>[] rowSet = new HashSet<char>[9];
+        HashSet<char>[] colSet = new HashSet<char>[9];
+        HashSet<char>[] sqSet = new HashSet<char>[9];
+        for (int i = 0; i < 9; i++)
+        {
+            rowSet[i] = new HashSet<char>();
+            colSet[i] = new HashSet<char>();
+            sqSet[i] = new HashSet<char>();
+        }
+        for(int i = 0; i < 9; i++)
+        {
+            for(int j = 0; j< 9; j++)
+            {
+                if(board[i][j] == '.')
+                {
+                    continue;
+                }
+                if(rowSet[i].Contains(board[i][j]))
+                {
+                    return false;
+                }
+                if(colSet[j].Contains(board[i][j]))
+                {
+                    return false;
+                }
+                int k = (3*(i/3)) + (j/3);
+                if(sqSet[k].Contains(board[i][j]))
+                {
+                    return false;
+                }
+                rowSet[i].Add(board[i][j]);
+                colSet[j].Add(board[i][j]);
+                sqSet[k].Add(board[i][j]);
+           }
+        }
+        return true;
+    }
+}
