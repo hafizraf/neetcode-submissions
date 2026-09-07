f73197adb5a23e2d4335f44e663e7a711458314c
Hafiz Rafeeq <91043653+hafizraf@users.noreply.github.com>
2026-09-07 21:08:48 +0530

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.cs
@@ -0,0 +1,30 @@
+public class Solution {
+    public bool IsValid(string s) {
+        Stack<char> stack = new Stack<char>(s.Length);
+        foreach(char c in s)
+        {
+            if(c == '(' || c == '[' || c == '{')
+            {
+                stack.Push(c);
+            }
+            else if(stack.Count == 0)
+            {
+                return false;
+            }
+            else if((c == ')' && stack.Pop() != '(') ||
+            (c == ']' && stack.Pop() != '[') ||
+            (c == '}' && stack.Pop() != '{') )
+            {
+                return false;
+            }
+        }
+        if(stack.Count == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
